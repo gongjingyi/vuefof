e1e9ffc0820037bde80ad31b535f89fcf6d7ab55
gongjingyi <zhangjingyi_zjy@163.com>
2018-01-07 23:31:31 +0800

    更新代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         'semi': ['error', 'always'],
         'no-tabs': 0,
         'indent': 0,
-        'space-before-function-paren': 0
+        'space-before-function-paren': 0,
+        'eol-last': 0
     }
 }
